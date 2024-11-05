ae79d7d0130b381a042196d26732d01d239036f9
Fadil Adzima <147174940+FadilAdz@users.noreply.github.com>
2024-11-05 14:12:51 +0700

    Add files via upload

---
+++ webdinamis/includes/contact.php
@@ -0,0 +1,24 @@
+<div class="p-2 bg-gradient"> 
+    <!-- Play Contact -->
+    <section class="text-center">
+        <div class="row justify-content-center fs-2 text-center">
+            <div class="col mt-2 text-secondary">
+                <p>
+                    <h1>
+                        <b>
+                            Contact Me
+                        </b>
+                    </h1>
+                    <a href="https://github.com/FadilAdz" target="_blank"><img src="img/github.svg" target="_blank" width="50" class="me-2"></a>
+                    <a href="https://www.instagram.com/fadiladz_/" target="_blank"><img src="img/instagram.svg" target="_blank" width="50" class="me-2"></a>
+                    <a href="https://discord.com" target="_blank"><img src="img/discord.svg" target="_blank" width="50" class="me-2"></a>
+                    <a href="https://Gmail.google.com/" target="_blank"><img src="img/email.svg" target="_blank" width="50" class="me-2"></a>
+                    <a href="https://Linkedin.com" target="_blank"><img src="img/linkedin.svg" target="_blank" width="50" class="me-2"></a>
+                    <!-- <a class="btn btn-secondary mt-5 " href="?page=about" role="button">About</a>
+                    <a class="btn btn-secondary mt-5 " href="?page=contact" role="button">Contact</a> -->
+                </p>
+            </div>
+        </div>
+    </section>
+    <!-- End Contact -->
+</div>
---
+++ webdinamis/includes/home.php
@@ -0,0 +1,24 @@
+<div class="p-2 bg-gradient"> 
+    <!-- Play Home -->
+    <section class="text-center">
+        <div class="row justify-content-center fs-2 text-center">
+            <div>
+                <p><img src="img/fa.jpg" width="200" alt="FadilAdz" class="rounded-circle img-thumbnail text-center mt-2"></p>
+            </div>
+            <div class="col mt-2 text-secondary">
+                <p>
+                    <h1>Fadil Aditya Adzima</h1>
+                    <h4 class="mb-3">Student | UI/UX Designer</h4>
+                    <a href="https://github.com/FadilAdz" target="_blank"><img src="img/github.svg" target="_blank" width="35" class="me-2"></a>
+                    <a href="https://www.instagram.com/fadiladz_/" target="_blank"><img src="img/instagram.svg" target="_blank" width="35" class="me-2"></a>
+                    <a href="https://discord.com" target="_blank"><img src="img/discord.svg" target="_blank" width="35" class="me-2"></a>
+                    <a href="https://Gmail.google.com/" target="_blank"><img src="img/email.svg" target="_blank" width="35" class="me-2"></a>
+                    <a href="https://Linkedin.com" target="_blank"><img src="img/linkedin.svg" target="_blank" width="35" class="me-2"></a>
+                    <!-- <a class="btn btn-secondary mt-5 " href="?page=about" role="button">About</a>
+                    <a class="btn btn-secondary mt-5 " href="?page=contact" role="button">Contact</a> -->
+                </p>
+            </div>
+        </div>
+    </section>
+    <!-- End Home -->
+</div>
